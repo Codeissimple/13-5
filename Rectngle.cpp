90d97b7bf6a450e024a18d2e02a2c9f87e19c657
Codeissimple <45950648+Codeissimple@users.noreply.github.com>
2021-09-12 02:31:29 +0000

    added class definition

---
+++ Rectngle.cpp
@@ -0,0 +1,48 @@
+
+// Class Implementation file 
+# include "Rectangle.h" // needed for the Rectangle class
+#include <iostream>     // Needed for cout
+#include <cstdlib>      //Needed for the exit function
+using namespace std;
+
+//The constructor accespts arguments for width and length
+/*
+Rectangle::Rectangle(double w, double len)
+{
+  width = w;
+  length = len;
+}
+*/
+//setWidth sets the value of the member variable width
+
+void Rectangle::setWidth(double w)
+{
+  if (w >=0)
+    width = w;
+  else
+  {
+    cout << "Invalid width\n";
+    exit(EXIT_FAILURE);
+  }
+}
+
+void Rectangle::setLength(double len)
+{
+  if (len >=0 )
+    length = len;
+  else
+  {
+    cout << "Invalid length\n";
+    exit(EXIT_FAILURE);
+  }
+}
+
+double Rectangle::getWidth() const
+{
+  
+}
+
+double Rectangle::getLength() const
+{
+  
+}
